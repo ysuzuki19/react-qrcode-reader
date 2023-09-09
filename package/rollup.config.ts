236326bebeaff9b0ef1c5336c60d4e361cf635bd
ysuzuki19 <ysuzuki.cpp@gmail.com>
2023-09-09 16:45:31 +0900

    rename rollup.config.js to rollup.config.ts

--- package/rollup.config.ts
+++ package/rollup.config.ts
@@ -1,9 +1,9 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import { dts } from 'rollup-plugin-dts';
-import babel from '@rollup/plugin-babel';
-import terser from '@rollup/plugin-terser';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 
 const entry = 'lib/index.tsx';
 
